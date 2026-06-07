018d218edb3d072702bcce53ed11c3385530ef7c
AmirHossein Jafari <amirhoseinjafari1386@gmail.com>
2026-06-07 15:09:49 +0330

    Dashboard directory Added

--- WebServer/WebServer.py
+++ WebServer/WebServer.py
@@ -2,14 +2,20 @@ from WebServer import app
 from flask import send_from_directory
 
 @app.route("/")
-
 def LoginPage ():
     return send_from_directory('LoginPage' , 'index.html')
 
 @app.route("/<path:filename>")
-
 def static_files(filename):
-    return send_from_directory("LoginPage", filename)
+    return send_from_directory("LoginPage", filename) 
 
+@app.route("/dashboard/")
+def Dashboard ():
+    return send_from_directory("Dashboard" , 'index.html')
+
+@app.route("/dashboard/<path:filename>")
+
+def dash_files(filename):
+    return send_from_directory("Dashboard", filename) 
 
 app.run()
