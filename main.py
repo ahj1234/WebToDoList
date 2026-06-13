e8696d98e7bb022d1f9d47bd25fd3de1c6cf4998
AmirHossein Jafari <amirhoseinjafari1386@gmail.com>
2026-06-13 08:47:04 +0330

    structure has changed

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import WebServer
+# from flask import send_from_directory
+from Classes import TO_DO_List
+from flask import Flask
+from flask import render_template
 
-def main():
-    return WebServer
+
+app = Flask(__name__)
+
+Current_List = TO_DO_List.To_Do()
+
+@app.route("/")
+def LoginPage ():
+    return render_template("LoginPage/index.html")
+
+@app.route("/Dashboard")
+def Dashboard ():
+    return render_template("/Dashboard/index.html")
+
+
+def main(debug=True):
+    return app.run()
     
 
 if __name__ == "__main__" :
